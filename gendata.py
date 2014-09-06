aae3fc3ca35099df31ae56bc09b37bacf6324316
Karol Guciek <k.guciek@gmail.com>
2014-09-06 22:58:20 +0200

    Merge files before embedding

--- gendata.py
+++ gendata.py
@@ -6,14 +6,10 @@
 
 import re, os, sys, jsstrip
 
-def addCodeForFile(result, fdiskpath, fid):
+def addCodeForFile(result, data, fid):
 	result.append("func ")
 	result.append(fid)
 	result.append("() []byte {\n    return []byte{\n        ")
-	with open(fdiskpath, "rb") as f:
-		data = f.read()
-	if fdiskpath[-3:] == ".js":
-		data = bytes(jsstrip.jsstrip(str(data, "utf-8")), "utf-8")
 	n = 0
 	for b in data:
 		result.append("%d,"%b)
@@ -23,21 +19,21 @@ def addCodeForFile(result, fdiskpath, fid):
 			result.append("\n        ")
 	result.append("\n    }\n}\n\n")
 
-def codeForDirectory(directory):
-	result = ["package data\n\n"]
+def codeForFiles(fdata):
 	array = []
-	for root, dirs, files in os.walk(directory):
-		for f in files:
-			fdiskpath = root+"/"+f
-			fpath = fdiskpath[len(directory)+1:]
-			fid = fpath.replace("/", "_").replace(".", "_")
-			fid = "".join([w.capitalize() for w in fid.split("_")])
-			addCodeForFile(result, fdiskpath, fid)
-			array.append("        \"")
-			array.append(fpath)
-			array.append("\": ")
-			array.append(fid)
-			array.append(",\n")
+	result = ["package data\n\n"]
+	for fpath in fdata:
+		fid = fpath.replace("/", "_").replace(".", "_")
+		fid = "".join([w.capitalize() for w in fid.split("_")])
+		data = fdata[fpath]
+		if fpath[-3:] == ".js":
+			data = bytes(jsstrip.jsstrip(str(data, "utf-8")), "utf-8")
+		addCodeForFile(result,  data, fid)
+		array.append("        \"")
+		array.append(fpath)
+		array.append("\": ")
+		array.append(fid)
+		array.append(",\n")
 	result.append("func ByPath(p string) []byte {\n")
 	result.append("    a := map[string]func()[]byte {\n")
 	result.append("".join(array))
@@ -45,9 +41,24 @@ def codeForDirectory(directory):
 	result.append("    return nil\n}\n\n")
 	return "".join(result)
 
+def generateForDirectory(directory):
+	fdata = dict()
+	for root, dirs, files in os.walk(directory):
+		for f in files:
+			fdiskpath = root+"/"+f
+			add_path = fdiskpath[len(directory)+1:]
+			add_path = re.sub("\[[^\]]+\]", "", add_path)
+			with open(fdiskpath, "rb") as f:
+				data = f.read()
+				if add_path in fdata:
+					fdata[add_path] += data
+				else:
+					fdata[add_path] = data
+	return codeForFiles(fdata)
+
 if __name__ == "__main__":
 	try:
-		d = codeForDirectory("data/files")
+		d = generateForDirectory("data/files")
 		with open(sys.argv[1], 'w') as f:
 			f.write(d)
 	except Exception as e:
